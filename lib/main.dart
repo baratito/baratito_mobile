99a57f5c75e38654ebb2a02fa5f1b894998d66e3
Francisco Pitriqueo <franpitri@gmail.com>
2021-08-05 01:18:15 -0300

    ✨ feat: add localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:baratito_ui/baratito_ui.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization_loader/easy_localization_loader.dart';
 import 'package:flutter/material.dart';
 
 import 'package:baratito_mobile/ui/splash/splash.dart';
 
-void main() {
-  runApp(ThemeProvider(child: const App()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    ThemeProvider(
+      child: EasyLocalization(
+        useOnlyLangCode: true,
+        supportedLocales: const [Locale('es')],
+        fallbackLocale: const Locale('es'),
+        path: 'assets/localizations',
+        assetLoader: YamlAssetLoader(),
+        child: const App(),
+      ),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -13,6 +28,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: _buildTheme(context),
       home: const SplashView(),
     );
@@ -20,6 +38,7 @@ class App extends StatelessWidget {
 
   ThemeData _buildTheme(BuildContext context) {
     return ThemeData(
+      fontFamily: 'packages/baratito_ui/Poppins',
       scaffoldBackgroundColor: context.theme.colors.background,
     );
   }
