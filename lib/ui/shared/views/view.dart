3451bb35165012e11ccefea46cbf55a75db59713
Francisco Pitriqueo <franpitri@gmail.com>
2021-08-07 15:38:31 -0300

    ♻️ refactor: add fab related attributes to View

--- lib/ui/shared/views/view.dart
+++ lib/ui/shared/views/view.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 
 class View extends StatelessWidget {
   final Widget child;
+  final Widget? bottomNavigationBar;
+  final Widget? fab;
+  final FloatingActionButtonLocation? fabLocation;
 
-  const View({Key? key, required this.child}) : super(key: key);
+  const View({
+    Key? key,
+    required this.child,
+    this.bottomNavigationBar,
+    this.fab,
+    this.fabLocation,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: bottomNavigationBar,
+      floatingActionButton: fab,
+      floatingActionButtonLocation: fabLocation,
       body: SafeArea(child: child),
     );
   }
