07c153e33f2455ee92d5c161c80e0ca59ba190ee
Francisco Pitriqueo <franpitri@gmail.com>
2021-08-13 00:40:44 -0300

    ♻️ refactor: add generic return types to NavigationExtension methods

--- lib/extensions/navigation_extension.dart
+++ lib/extensions/navigation_extension.dart
@@ -8,41 +8,45 @@ enum RouteTransitionType {
 extension NavigationExtension on BuildContext {
   NavigatorState get navigator => Navigator.of(this);
 
-  Future<T> pushView<T>(
+  Future<T?> pushView<T>(
     Widget view, [
     RouteTransitionType transitionType = RouteTransitionType.systemDefault,
   ]) async {
-    return await navigator.push(_buildRoute(view, transitionType));
+    return navigator.push<T>(_buildRoute<T>(view, transitionType));
   }
 
-  Future<T> pushReplacementView<T>(
+  Future<T?> pushReplacementView<T>(
     Widget view, [
     RouteTransitionType transitionType = RouteTransitionType.systemDefault,
   ]) async {
-    return await navigator.pushReplacement(_buildRoute(view, transitionType));
+    return navigator.pushReplacement<T, T>(
+      _buildRoute<T>(view, transitionType),
+    );
   }
+
+  Future<bool> popView<R>({R? result}) async => navigator.maybePop(result);
 }
 
-Route _buildRoute(
+Route<T> _buildRoute<T>(
   Widget view, [
   RouteTransitionType transitionType = RouteTransitionType.systemDefault,
 ]) {
   final settings = RouteSettings(name: '${view.runtimeType}');
   switch (transitionType) {
     case RouteTransitionType.systemDefault:
-      return MaterialPageRoute(
+      return MaterialPageRoute<T>(
         settings: settings,
         builder: (context) => view,
       );
     case RouteTransitionType.fade:
-      return FadeRoute(
+      return FadeRoute<T>(
         routeSettings: settings,
         view: view,
       );
   }
 }
 
-class FadeRoute extends PageRouteBuilder {
+class FadeRoute<T> extends PageRouteBuilder<T> {
   final Widget view;
   final RouteSettings? routeSettings;
 
