e26b80d4f753cc0042539fefe2e147122fc826cd
Francisco Pitriqueo <franpitri@gmail.com>
2021-11-28 17:18:56 -0300

    ♻️ refactor: add isScrollControlled attribute to showBottomSheet extension

--- lib/extensions/bottom_sheets_extension.dart
+++ lib/extensions/bottom_sheets_extension.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 extension BottomSheetExtension on BuildContext {
-  Future<T?> showBottomSheet<T>(Widget child) async {
+  Future<T?> showBottomSheet<T>(
+    Widget child, {
+    bool isScrollControlled = false,
+  }) async {
     return showModalBottomSheet<T>(
       context: this,
       backgroundColor: Colors.transparent,
       builder: (context) => child,
+      isScrollControlled: isScrollControlled,
     );
   }
 }
